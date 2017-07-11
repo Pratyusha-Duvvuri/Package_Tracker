cf2df05d03fa4f142f66cdc2b94987663b1358f2
Pratyusha Duvvuri <pratyusha98@fb.com>
2017-07-11 11:20:48 -0700

    Removed User class

--- app/src/main/java/com/codepath/packagetwitter/Models/Sender.java
+++ app/src/main/java/com/codepath/packagetwitter/Models/Sender.java
@@ -4,7 +4,7 @@ package com.codepath.packagetwitter.Models;
  * Created by pratyusha98 on 7/11/17.
  */
 
-public class Sender extends User {
+public class Sender {
 
     Double location;
     Boolean hasSent;
