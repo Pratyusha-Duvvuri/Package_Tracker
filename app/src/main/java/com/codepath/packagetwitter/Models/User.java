fcbc449759b63627314164b7abea28581455e816
Rafael Jacobovitz <rafasj6@fb.com>
2017-07-11 13:17:13 -0700

    Transactiions activity

--- app/src/main/java/com/codepath/packagetwitter/Models/User.java
+++ app/src/main/java/com/codepath/packagetwitter/Models/User.java
@@ -1,5 +1,7 @@
 package com.codepath.packagetwitter.Models;
 
+import java.util.ArrayList;
+
 /**
  * Created by michaunp on 7/11/17.
  */
@@ -10,5 +12,15 @@ public class User {
     String phoneNum;
     String tripStart;
     String tripEnd;
+    ArrayList <Transaction> transactions;
 
+    public User (ArrayList<Transaction> transacts){
+        transactions = transacts;
+
+
+    }
+
+    public ArrayList<Transaction> getTransactions() {
+        return transactions;
+    }
 }
--- app/src/main/java/com/codepath/packagetwitter/TransactionsActivity.java
+++ app/src/main/java/com/codepath/packagetwitter/TransactionsActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.codepath.packagetwitter.Models.Transaction;
+import com.codepath.packagetwitter.Models.User;
 
 import java.util.ArrayList;
 
@@ -15,11 +16,15 @@ public class TransactionsActivity extends AppCompatActivity {
     TransactionAdapter adapter;
     RecyclerView rvTransactions;
 
+    User user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transactions);
-        transactions = new ArrayList<>();
+        user = (User) getIntent().getParcelableExtra("USER");
+        transactions = user.getTransactions();
+
 
         adapter = new TransactionAdapter(transactions);
 
@@ -27,7 +32,7 @@ public class TransactionsActivity extends AppCompatActivity {
         rvTransactions.setLayoutManager(new LinearLayoutManager(this));
         rvTransactions.setAdapter(adapter);
 
-        //get config
+
 
     }
 }
