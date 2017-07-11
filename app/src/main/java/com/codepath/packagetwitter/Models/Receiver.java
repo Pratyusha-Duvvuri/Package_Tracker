778c83a08040c8e94f7d6888bda5dd33127803fd
Pratyusha Duvvuri <pratyusha98@fb.com>
2017-07-11 10:48:05 -0700

    Added sender and receiver classes

---
+++ app/src/main/java/com/codepath/packagetwitter/Models/Receiver.java
@@ -0,0 +1,11 @@
+package com.codepath.packagetwitter.Models;
+
+/**
+ * Created by pratyusha98 on 7/11/17.
+ */
+
+public class Receiver extends User{
+
+    Boolean hasReceived;
+    Double location;
+}
---
+++ app/src/main/java/com/codepath/packagetwitter/Models/Sender.java
@@ -0,0 +1,12 @@
+package com.codepath.packagetwitter.Models;
+
+/**
+ * Created by pratyusha98 on 7/11/17.
+ */
+
+public class Sender extends User {
+
+    Double location;
+    Boolean hasSent;
+
+}
