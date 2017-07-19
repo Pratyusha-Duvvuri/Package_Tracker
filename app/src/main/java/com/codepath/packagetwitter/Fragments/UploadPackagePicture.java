f7c4dadb9b1f7281ab3798e776e7fa99cb1d0d43
Pratyusha Duvvuri <pratyusha98@fb.com>
2017-07-19 16:06:45 -0700

    saving changes

---
+++ app/src/main/java/com/codepath/packagetwitter/Fragments/UploadPackagePicture.java
@@ -0,0 +1,10 @@
+package com.codepath.packagetwitter.Fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by pratyusha98 on 7/19/17.
+ */
+
+public class UploadPackagePicture extends Fragment {
+}
