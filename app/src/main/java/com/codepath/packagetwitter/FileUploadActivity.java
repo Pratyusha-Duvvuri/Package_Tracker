8ba8d2165c87d1af0673b1c477d4d16bfc8ca0fa
Pratyusha Duvvuri <pratyusha98@fb.com>
2017-07-21 09:18:31 -0700

    Fixed error in images :) Now profile image is uploaded successfully

--- app/src/main/java/com/codepath/packagetwitter/FileUploadActivity.java
+++ app/src/main/java/com/codepath/packagetwitter/FileUploadActivity.java
@@ -168,9 +168,23 @@ public class FileUploadActivity extends Activity {
                 byte[] image = stream.toByteArray();
 
                 // Create the ParseFile
-                ParseFile file = new ParseFile(caption.getText().toString(), image);
-                // Upload the image into Parse Cloud
-                file.saveInBackground();
+                ParseFile file = new ParseFile(parseUser.getString("username"), image);
+
+                file.saveInBackground(new SaveCallback(){
+                    @Override
+                    public void done(ParseException e1) {
+                        if (e1 == null) {
+                            Log.d("FOR THE LOVE OF GOD","yass");
+                            Toast.makeText(FileUploadActivity.this, "Image Saved",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.d("UMM","THEFISH");
+                            Log.d("ParseApplicationError",e1.toString());
+                            Toast.makeText(FileUploadActivity.this, "NOT Saved",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
                 // Create a New Class called "ImageUpload" in Parse
                 //ParseObject imgupload = new ParseObject("ImageUpload");
@@ -181,11 +195,7 @@ public class FileUploadActivity extends Activity {
                 // Create a column named "ImageFile" and insert the image
                 parseUser.put("ImageFile", file);
 
-                // Create the class and the columns
-//                parseUser.saveInBackground();
-
-
-                file.saveInBackground(new SaveCallback(){
+                parseUser.saveInBackground(new SaveCallback(){
                     @Override
                     public void done(ParseException e1) {
                         if (e1 == null) {
@@ -201,187 +211,10 @@ public class FileUploadActivity extends Activity {
                     }
                 });
 
+
             }
-
-                // Show a simple toast message
-
 
             }
         }
     }
 
-//    // Returns the Uri for a photo stored on disk given the fileName
-//    public Uri getPhotoFileUri(String fileName) {
-//        // Only continue if the SD Card is mounted
-//        if (isExternalStorageAvailable()) {
-//            // Get safe storage directory for photos
-//            // Use `getExternalFilesDir` on Context to access package-specific directories.
-//            // This way, we don't need to request external read/write runtime permissions.
-//            File mediaStorageDir = new File(
-//                    getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
-//
-//            // Create the storage directory if it does not exist
-//            if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
-//                Log.d(APP_TAG, "failed to create directory");
-//            }
-//
-//            // Return the file target for the photo based on filename
-//            File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
-//
-//            // wrap File object into a content provider
-//            // required for API >= 24
-//            // See https://guides.codepath.com/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
-//            return FileProvider.getUriForFile(FileUploadActivity.this, "com.codepath.fileprovider", file);
-//        }
-//        return null;
-//    }
-//
-//    // Returns true if external storage for photos is available
-//    private boolean isExternalStorageAvailable() {
-//        String state = Environment.getExternalStorageState();
-//        return state.equals(Environment.MEDIA_MOUNTED);
-//    }
-
-//
-//    private File createImageFile() throws IOException {
-//        // Create an image file name
-//        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-//        String imageFileName = "JPEG_" + timeStamp + "_";
-//        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-//        File image = File.createTempFile(
-//                imageFileName,  /* prefix */
-//                ".jpg",         /* suffix */
-//                storageDir      /* directory */
-//        );
-//
-//        // Save a file: path for use with ACTION_VIEW intents
-//        mCurrentPhotoPath = image.getAbsolutePath();
-//        return image;
-//    }
-
-
-
-
-
-
-    /*
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // Get the view from activity_file_upload.xml_file_upload.xml
-        setContentView(R.layout.activity_file_upload);
-
-        // Locate the button in activity_file_uploadvity_file_upload.xml
-        //button = (Button) findViewById(R.id.uploadbtn);
-        btnUploadson = (Button) findViewById(R.id.btnuploadson);
-        caption = (EditText) findViewById(R.id.et_imagecaption);
-        GoBack = (Button) findViewById(R.id.btnBackToProfile);
-
-        btnUploadson.setOnClickListener(new OnClickListener() {
-
-            public void onClick(View view) {
-                // Locate the image in res > drawable-hdpi
-                onLaunchCamera(view);
-
-                // Show a simple toast message
-
-            }
-        });
-        GoBack.setOnClickListener(new OnClickListener() {
-
-            public void onClick(View view) {
-                // Locate the image in res > drawable-hdpi
-                Intent i = new Intent(FileUploadActivity.this, ProfileActivity.class);
-                i.putExtra("PARSEUSER", ProfileActivity.parseUser.getObjectId());
-                setResult(RESULT_OK, i); // set result code and bundle data for response
-                finish(); // closes the activity, pass data to parent
-
-            }
-        });
-        ivPreview = (ImageView) findViewById(R.id.ivPreview);
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.timeline_menu, menu);
-        return true;
-    }
-
-    //Following functions are to launch the camera and take a photo
-    @NeedsPermission(Manifest.permission.CAMERA	)
-    public void onLaunchCamera(View view) {
-        // create Intent to take a picture and return control to the calling application
-        Intent takepicintent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-        // So as long as the result is not null, it's safe to use the intent.
-        if (takepicintent.resolveActivity(getPackageManager()) != null) {
-            // Start the image capture intent to take photo
-            startActivityForResult(takepicintent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-        }
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-               Bundle extras = data.getExtras();
-                Bitmap bitmap = (Bitmap) extras.get("data");
-                ivPreview.setImageBitmap(bitmap);
-
-                // Convert it to gibyte
-                ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                // Compress image to lower quality scale 1 - 100
-                bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-                byte[] image = stream.toByteArray();
-
-                // Create the ParseFile
-                 file = new ParseFile(caption.getText().toString(), image);
-                // Upload the image into Parse Cloud
-//                file.saveInBackground();
-//            public void done(ParseException e) {
-//                if (e == null) {
-//                    myObjectSavedSuccessfully();
-//                } else {
-//                    myObjectSaveDidNotSucceed();
-//                }
-//            }
-// });
-                parseUser.put("ImageName", "MEAW");
-                parseUser.put("TF","tf");
-                ProfileActivity.parseUser.put("ImageFile", file);
-
-            file.saveInBackground(new SaveCallback(){
-                @Override
-                public void done(ParseException e1) {
-                    if (e1 == null) {
-                        parseUser.saveEventually();
-
-                        myObjectSavedSuccessfully();
-                    } else {
-                        Log.d("UMM","THEFISH");
-                        Log.d("ParseApplicationError",e1.toString());
-                        Toast.makeText(FileUploadActivity.this, "NOT Uploaded",
-                                Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-
-            }
-
-        }
-    }
-    public void myObjectSavedSuccessfully(){
-
-
-        // Show a simple toast message
-        Toast.makeText(FileUploadActivity.this, "Image Uploaded",
-                Toast.LENGTH_SHORT).show();
-
-
-    }
-
-}
-*/
