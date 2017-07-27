7f604a45cb190c931567528a72d636df4f116399
Michaun Pierre <michaunp@fb.com>
2017-07-26 23:09:13 -0700

    Working on page 2 of package creation activity

--- app/src/main/java/com/codepath/packagetwitter/PackageCreationPart2Activity.java
+++ app/src/main/java/com/codepath/packagetwitter/PackageCreationPart2Activity.java
@@ -1,10 +1,15 @@
 package com.codepath.packagetwitter;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.CardView;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
@@ -14,6 +19,7 @@ import com.parse.ParseException;
 import com.parse.ParseQuery;
 
 import butterknife.BindView;
+
 
 /**
  * Created by pratyusha98 on 7/25/17.
@@ -41,12 +47,14 @@ public class PackageCreationPart2Activity extends Activity {
     @BindView(R.id.ibPhone) ImageButton ibPhone;
     @BindView(R.id.ibBookSized) ImageButton ibBookSized;
     @BindView(R.id.ibFishBowl) ImageButton ibFishBowl;
+    @BindView(R.id.llType) LinearLayout llType;
+    @BindView(R.id.llSize) LinearLayout llSize;
+    @BindView(R.id.cvPackageType) CardView cvPackageType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //set up actionbar
-
+        final Context context = this;
         // Get the view from activity_file_upload.xml_file_upload.xml
         setContentView(R.layout.package_creation_part2);
         String parselTransactionID = getIntent().getStringExtra("TRANSACTION");
@@ -59,8 +67,36 @@ public class PackageCreationPart2Activity extends Activity {
             public void done(ParselTransaction item, ParseException e) {
                 trans = item;
                 if (e == null) {
+                    Log.d("WORK", "inside query");
+                    trans.put("title", etTitle.toString());
+                    trans.setMailDescription(etDescription.toString());
+                    trans.setWeight(Double.parseDouble(etWeight.toString()));
 
-
+                    cvPackageType.setOnClickListener(new View.OnClickListener() {
+                        boolean clothes_outline = true;
+                        int icon;
+                        @Override
+                        public void onClick(View view) {
+                            Log.d("WORK", String.valueOf(view.getId()));
+                            Log.d("WORK", String.valueOf(R.id.llClothes));
+                            Log.d("WORK", String.valueOf(R.id.ibClothes));
+                            switch (view.getId()) {
+                                case R.id.llClothes:
+                                    if (clothes_outline) {
+                                        clothes_outline = false;
+                                        icon = R.mipmap.ic_clothes_fill;
+                                    }
+                                    else {
+                                        clothes_outline = true;
+                                        icon = R.mipmap.ic_clothes;
+                                    }
+                                    ibClothes.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), icon));
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    });
                 }
 
                 else {
@@ -68,6 +104,8 @@ public class PackageCreationPart2Activity extends Activity {
                 }
             }
         });
+
+
     }
 
 }
