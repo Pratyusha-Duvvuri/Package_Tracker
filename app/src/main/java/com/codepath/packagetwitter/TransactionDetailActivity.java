58a5c2f09e254f209b593cae0b2b9b2b329db172
Michaun Pierre <michaunp@fb.com>
2017-08-04 16:46:00 -0700

    Working on tracker

--- app/src/main/java/com/codepath/packagetwitter/TransactionDetailActivity.java
+++ app/src/main/java/com/codepath/packagetwitter/TransactionDetailActivity.java
@@ -220,10 +220,44 @@ public class TransactionDetailActivity extends AppCompatActivity implements Vert
             confirm_button.setVisibility(View.GONE);
 
         if (parselTransactionState == 4){
-            verticalStepperForm.setStepAsCompleted(0);
-            verticalStepperForm.setStepAsCompleted(1);
-            verticalStepperForm.setStepAsCompleted(2);
-            verticalStepperForm.setStepAsCompleted(3);
+            alreadyExecuted = true;
+            switch(verticalStepperForm.getActiveStepNumber()){
+                case 0:
+                    verticalStepperForm.setStepAsCompleted(1);
+                    verticalStepperForm.setStepAsCompleted(2);
+                    verticalStepperForm.setStepAsCompleted(3);
+                    verticalStepperForm.goToStep(1, false);
+                    verticalStepperForm.setStepAsCompleted(0);
+                    break;
+                case 1:
+                    verticalStepperForm.setStepAsCompleted(0);
+                    verticalStepperForm.setStepAsCompleted(2);
+                    verticalStepperForm.setStepAsCompleted(3);
+                    verticalStepperForm.goToStep(2, false);
+                    verticalStepperForm.setStepAsCompleted(1);
+                    break;
+                case 2:
+                    verticalStepperForm.setStepAsCompleted(0);
+                    verticalStepperForm.setStepAsCompleted(1);
+                    verticalStepperForm.setStepAsCompleted(3);
+                    verticalStepperForm.goToStep(3, false);
+                    verticalStepperForm.setStepAsCompleted(2);
+                    break;
+                case 3:
+                    verticalStepperForm.setStepAsCompleted(0);
+                    verticalStepperForm.setStepAsCompleted(1);
+                    verticalStepperForm.setStepAsCompleted(2);
+                    verticalStepperForm.goToStep(4, false);
+                    verticalStepperForm.setStepAsCompleted(3);
+                    break;
+                default:
+                    break;
+
+            }
+//            verticalStepperForm.setStepAsCompleted(0);
+//            verticalStepperForm.setStepAsCompleted(1);
+//            verticalStepperForm.setStepAsCompleted(2);
+//            verticalStepperForm.setStepAsCompleted(3);
             alreadyExecuted = true;
         }
         else{
@@ -235,7 +269,10 @@ public class TransactionDetailActivity extends AppCompatActivity implements Vert
                 verticalStepperForm.setStepAsCompleted(0);
                 verticalStepperForm.setStepAsCompleted(1);
                 verticalStepperForm.setStepAsCompleted(2);
-                verticalStepperForm.setStepAsCompleted(3);
+                if (verticalStepperForm.getActiveStepNumber() != 3){
+                    //verticalStepperForm.goToStep(4, false);
+                    verticalStepperForm.setStepAsCompleted(3);
+                }
                 sendData();
                 confirm_button.setVisibility(View.GONE);
 
@@ -347,6 +384,8 @@ public class TransactionDetailActivity extends AppCompatActivity implements Vert
 
     public final void onStepOpening(int stepNumber) {
 
+        if (parselTransactionState == 4)
+            alreadyExecuted = true;
         if (!alreadyExecuted){
             switch (stepNumber){
                 case 0:
